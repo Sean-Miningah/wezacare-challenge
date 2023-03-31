214f88fad60356782e4d37a07617eb6c3aea5d5f
Sean-Miningah <miningahsean@gmail.com>
2023-03-31 23:05:46 +0300

    refactor

--- questions/views.py
+++ questions/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth import authenticate
 from django.db.models import Prefetch
 from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
